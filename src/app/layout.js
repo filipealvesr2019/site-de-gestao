74d21cddedd24cf2a2ae52d4d3c5d4e87c30981b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-11-14 09:21:49 -0300

    feat: adiciona logica de adiciona receitas e despesas

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { ptBR } from '@clerk/localizations'
 
 // Certifique-se de envolver o ClerkProvider ao redor de todo o conteúdo
 export default function RootLayout({ children }) {
+  
   return (
-    <ClerkProvider dynamic>
+    <ClerkProvider dynamic localization={ptBR}>
 
           <html lang="en">
         <body>
