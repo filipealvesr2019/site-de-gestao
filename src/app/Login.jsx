0e1c91badc9887642fe847fb154139b4cd233c0b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-11-28 11:52:07 -0300

    refatora: refatora o css da a pagina de receitas e despesas.

--- src/app/Login.jsx
+++ src/app/Login.jsx
@@ -1,36 +1,37 @@
 "use client";
+
+import styles from "./Login.module.css";
+import { useState } from "react";
+import Link from "next/link";
 import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs";
-import styles from './Login.module.css'
+
 export default function Login() {
+  const [showSignIn, setShowSignIn] = useState(false);
+
   return (
     <>
-     <div  className={styles.login}>
-<p>LOGO</p>
-<SignIn/>
+      <div className={styles.login}>
+        <p>LOGO</p>
+
+   
       <SignedOut>
-        <SignInButton >
-          {/* Personalize o botão com classes ou estilos inline */}
-          <button
-           className={styles.button}
-          >
-            Entrar
-          </button>
-        </SignInButton>
+     
+        <Link href={"/login"}>
+          <button className={styles.button}>Entrar</button>
+        </Link>
+   
       </SignedOut>
       <SignedIn>
         {/* Exibe o botão do usuário autenticado */}
         <UserButton />
       </SignedIn>
 
-      {/* Componente protegido que só será exibido se o usuário estiver autenticado */}
-      <SignedIn>
-
-      </SignedIn>
+      
 
       {/* Exibe uma mensagem ou redireciona para login quando o usuário não estiver autenticado */}
       <SignedOut>
       </SignedOut>
-     </div>
+      </div>
     </>
   );
 }
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,14 @@
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function SignInComponente(){
+    return (
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            height:"100vh",
+            alignItems:"center"
+        }}>
+        <SignIn/>
+        </div>
+    )
+}
