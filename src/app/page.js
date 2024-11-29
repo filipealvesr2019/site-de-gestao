2af329eac51df0b93a0b910e3bf77166234d0287
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-11-29 09:11:44 -0300

    feat: adiciona middleware de proteção de rotas

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,42 @@
-
 import styles from "./page.module.css";
 import Login from "./Login";
 import UserAuth from "@/components/UserAuth/UserAuth";
+import Script from "next/script";
 
 export default function Home() {
-  
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "Nome do site",
+    url: "https://meusite.com.br/",
+    description:
+      "Sistema online de controle financeiro simples e eficiente para gerenciar receitas e despesas. Ele permite que você controle tudo o que entra e sai do caixa, desde vendas e pagamentos até contas e dívidas.",
+    potentialAction: {
+      "@type": "SearchAction",
+      target: "https://meusite.com.br/search?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+  };
+
   return (
-    <div className={styles.container} >
-
-<Login />
-<UserAuth />
-
+    <div className={styles.container}>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?idG-sdsdsdsd`} // Substitua pelo seu Measurement ID
+      />
+      <Script id="google-analytics-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date()); 
+          gtag('config', 'G-dsdsdsds', { page_path: window.location.pathname });`}
+      </Script>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <Login />
+      <UserAuth />
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,17 +3,17 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <div>
-        
+        <div className={styles.div}>
+        <p>pra ajudar o projeto:</p>
           <img
-            src="https://i.imgur.com/VEVrhxh.jpg"
+            src="https://i.imgur.com/NhUTIMl.jpg"
             alt="QR Code para ajudar o projeto"
           />
-            <p>pra ajudar o projeto:</p>
+  
         </div>
-        <section>
+        <section className={styles.section}>
           <h2>Sobre</h2>
-          <p>Este é um sistema de controle financeiro simples e eficiente para gerenciar receitas e despesas. Ele permite que você controle tudo o que entra e sai do caixa, desde vendas e pagamentos até contas e dívidas.
+          <p>Sistema online de controle financeiro simples e eficiente para gerenciar receitas e despesas. Ele permite que você controle tudo o que entra e sai do caixa, desde vendas e pagamentos até contas e dívidas.
           </p>
         </section>
 
