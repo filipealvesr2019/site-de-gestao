0f803dad95dac9403579a5226f3bb3a8fd67a810
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-11-30 15:17:20 -0300

    feat: adiciona middleware de proteção de rotas

--- src/components/UserAuth/UserAuth.jsx
+++ src/components/UserAuth/UserAuth.jsx
@@ -1,8 +1,7 @@
 "use client";
 import { useAuth } from '@clerk/nextjs';
 import ProductList from "../ProductList/ProductList";
-import dynamic from 'next/dynamic';
-const ProductList = dynamic(() => import('../LandinPage/LandinPage'), { ssr: false });
+import LandinPage from "../LandinPage/LandinPage";
 
 export default function UserAuth() {
     const { isSignedIn } = useAuth();
