5ff29a81e10feacd0b23cd60485a3abf3990d97c
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-03 09:30:34 -0300

    feat: adiciona cors

--- pages/api/products.js
+++ pages/api/products.js
@@ -75,7 +75,7 @@ export default async function handler(req, res) {
         }
     
         // Consultar todos os produtos
-        const products = await Product.find({ userId }).sort({ dataCriacao: -1 });
+        const products = await Product.find({ userId: "fdsfsdf" }).sort({ dataCriacao: -1 });
         // Retornar os produtos e o total de receitas pagas
         res.status(200).json({ products });
       } catch (error) {
