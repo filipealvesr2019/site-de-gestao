e5858333a96636dd7ba13112f9ca044f630e0fce
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-05 09:18:51 -0300

    feat: adiciona css do modal de excluir receita ou despesas

--- pages/api/cronJobs.js
+++ pages/api/cronJobs.js
@@ -26,7 +26,7 @@ const checkAndUpdateProductsStatus = async () => {
 if (typeof window === "undefined") {
   // Este código será executado apenas no lado do servidor
 
-  cron.schedule('0 0 * * *', checkAndUpdateProductsStatus);
+  cron.schedule('*/30 * * * * *', checkAndUpdateProductsStatus);
 
 }
 
