445b638a727b9767f30d3132a907b789a7c52c83
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-05 10:56:51 -0300

    refactor: refatora o css dos cards de receitas e despesas

--- src/app/login/[[...rest]]/page.js
+++ src/app/login/[[...rest]]/page.js
@@ -2,8 +2,13 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function login(){
     return (
-        <>
+        <div style={{
+            display:"flex",
+            justifyContent:"center",
+            height:"100vh",
+            alignItems:"center"
+        }}>
         <SignIn />
-        </>
+        </div>
     )
 }
