695097e1e067072ea412f7144ee1bcdaa00bc2f5
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-13 11:26:37 -0300

    refactor: refatora o css da landing page

--- src/components/LandinPage/LandinPage.jsx
+++ src/components/LandinPage/LandinPage.jsx
@@ -1,4 +1,3 @@
-import { SignedOut, SignInButton } from "@clerk/nextjs";
 import styles from "./LandinPage.module.css";
 import Footer from "../Footer/Footer";
 import Link from "next/link";
@@ -12,20 +11,91 @@ export default function LandinPage() {
             "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
         }}
       >
-        <div className={styles.containerA} >
+        <div className={styles.containerA}>
           <img
             src="https://i.imgur.com/VNPEJWh.png"
             alt=""
             className={styles.containerA__img}
           />
           <div className={styles.containerA__texts}>
-            <h1 >Pare de Perder o Controle das Suas Finanças</h1>
+            <h1>Gerencie Suas Finanças com Facilidade e Eficiência</h1>
 
             <p className={styles.p}>
-            Sabemos como é difícil organizar todas as vendas, pagamentos e contas do dia a dia. Por isso, criamos uma ferramenta de controle financeiro simples e gratuita para que você acompanhe tudo o que entra e sai do seu caixa, de forma organizada e sem esforço.       </p>
+              Surgindo da necessidade de uma solução prática e eficiente
+              paragerenciar suas finanças com facilidade.
+            </p>
+          </div>
+        </div>
+        <div className={styles.containerF}>
+          <img
+            src="https://i.imgur.com/4dTM0Kj.png"
+            alt=""
+            className={styles.containerA__img}
+          />
+          <div className={styles.containerA__texts}>
+            <h1>Como Funciona?</h1>
+
+            <p className={styles.p}>
+              Acesse o Sistema: Sem cadastro complicado, direto pelo navegador.
+              Registre Suas Receitas e Despesas: Informe suas vendas,
+              pagamentos, contas e dívidas. Visualize Seu Fluxo de Caixa:
+              Controle tudo de forma simples e organizada.
+            </p>
           </div>
         </div>
 
+        <div className={styles.containerE}>
+          <div className={styles.pointsContainer}>
+            <div className={styles.points}>1.</div>
+            <div className={styles.containerE__texts}>
+              <h1 className={styles.containerE__H1}>Simplicidade Total</h1>
+              <p className={styles.containerE__P}>
+                Interface intuitiva, fácil de usar – mesmo para quem não tem
+                experiência.
+              </p>
+            </div>
+          </div>
+          <div className={styles.pointsContainer}>
+            <div className={styles.points}>2.</div>
+            <div className={styles.containerE__texts}>
+              <h1 className={styles.containerE__H1}>
+                Controle de Receitas e Despesas
+              </h1>
+              <p className={styles.containerE__P}>
+                Acompanhe tudo o que entra e sai do seu caixa.
+              </p>
+            </div>
+          </div>
+          <div className={styles.pointsContainer}>
+            <div className={styles.points}>3.</div>
+            <div className={styles.containerE__texts}>
+              <h1 className={styles.containerE__H1}>
+                Gerenciamento de Contas e Dívidas
+              </h1>
+              <p className={styles.containerE__P}>
+                Nunca mais perca prazos ou se desorganize com pagamentos.
+              </p>
+            </div>
+          </div>
+          <div className={styles.pointsContainer}>
+            <div className={styles.points}>4.</div>
+            <div className={styles.containerE__texts}>
+              <h1 className={styles.containerE__H1}>Online e Gratuito</h1>
+              <p className={styles.containerE__P}>
+                Acesse direto do site, sem downloads ou assinaturas.
+              </p>
+            </div>
+          </div>
+          <div className={styles.pointsContainer}>
+            <div className={styles.points}>5.</div>
+            <div className={styles.containerE__texts}>
+              <h1 className={styles.containerE__H1}>Eficiência Garantida</h1>
+              <p className={styles.containerE__P}>
+                Veja suas finanças de forma clara e tome decisões melhores.
+              </p>
+            </div>
+          </div>
+        </div>
         <div className={styles.containerB}>
           <h1 className={styles.heading}>
             Cadastre Serviços, Produtos e Controle Tudo o que Entra e Sai
@@ -37,7 +107,6 @@ export default function LandinPage() {
             className={styles.container__product__img}
           />
         </div>
-
 
         <div className={styles.containerC}>
           <div className={styles.containerC__texts}>
@@ -53,20 +122,17 @@ export default function LandinPage() {
         </div>
 
         <div className={styles.containerD}>
-          <div  className={styles.containerD__texts}>
-            <h1>
-            Gerencie Suas Finanças Gratuitamente
-            </h1>
+          <div className={styles.containerD__texts}>
+            <h1>E o melhor: tudo isso de forma totalmente gratuita!</h1>
             <p className={styles.paragraph}>
-            Controle suas finanças, organize seu fluxo de caixa e tenha mais tempo para focar no que realmente importa. Simples, online e 100% gratuito.            </p>
+              Cadastre-se agora e aproveite todas as funcionalidades sem nenhum
+              custo.
+            </p>
           </div>
           <Link href={"/login"}>
-          <div>
-        
-                <button className={styles.button}>Cadastre-se</button>
-          
-          </div>
-
+            <div>
+              <button className={styles.button}>Cadastre-se</button>
+            </div>
           </Link>
         </div>
       </main>
