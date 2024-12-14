1b798cfade3ad4d7f3f317750e8b9754530469ee
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-14 10:25:55 -0300

    refactor: refatora o css da landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/:path*', // Caminho relativo, capturando todas as rotas
-            destination: (req) => {
-              // Verifique se o domínio não é o de destino
-              if (req.headers.host === 'www.gestaofinanceirapro.online') {
-                return 'https://www.gestaofinanceirapro.com.br/:path*';
-              }
-              return req.url; // Se já estiver no destino, não faz o redirecionamento
-            },
-            permanent: true, // Redirecionamento permanente (status 301)
-          },
-        ]
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
