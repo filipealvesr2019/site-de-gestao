42adc72f7180082d6384829d1b3adca6ffc923ca
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-14 10:30:28 -0300

    refactor: refatora o css da landing page

--- middleware.js
+++ middleware.js
@@ -1,18 +1,39 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// const isProtectedRoute = createRouteMatcher(['/api/products(.*)', '/api/receitas(.*)', '/api/despesas(.*)', '/api/diferenca(.*)', '/api/filtrar(.*)'])
-
-const isProtectedRoute = createRouteMatcher(['/api/products(.*)', '/api/receitas(.*)', '/api/despesas(.*)', '/api/diferenca(.*)', '/api/filtrar(.*)', '/api/reports(.*)' ])
+const isProtectedRoute = createRouteMatcher([
+    '/api/products(.*)',
+    '/api/receitas(.*)',
+    '/api/despesas(.*)',
+    '/api/diferenca(.*)',
+    '/api/filtrar(.*)',
+    '/api/reports(.*)',
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
-})
+    // Verifica se o hostname é o que você deseja redirecionar
+    const { hostname } = req.nextUrl;
+
+    if (hostname === 'www.gestaofinanceirapro.online') {
+        const url = req.nextUrl.clone();
+        url.hostname = 'www.gestaofinanceirapro.com.br';
+        return NextResponse.redirect(url, 301); // Redirecionamento permanente
+    }
+
+    // Protege as rotas se necessário
+    if (isProtectedRoute(req)) {
+        await auth.protect();
+    }
+
+    // Permite continuar com a requisição
+    return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+    matcher: [
+        // Ignora arquivos internos do Next.js e todos os arquivos estáticos
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Sempre executa para rotas da API
+        '/(api|trpc)(.*)',
+    ],
+};
