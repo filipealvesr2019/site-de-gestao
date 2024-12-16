946e0173fca40116b2718921efb2ffd10dd371ff
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-16 09:18:23 -0300

    refactor: adiciona adiciona politica de privacidade

---
+++ src/app/privacidade/page.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const PoliticaDePrivacidade = () => {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Política de Privacidade</h1>
+      <p>
+        Bem-vindo à nossa Política de Privacidade. Sua privacidade é importante para nós. Este documento explica como coletamos, usamos e protegemos suas informações pessoais.
+      </p>
+
+      <h2>Coleta de Informações</h2>
+      <p>
+        Coletamos informações fornecidas por você, como nome, e-mail e outras informações necessárias para fornecer nossos serviços.
+      </p>
+
+      <h2>Uso das Informações</h2>
+      <p>
+        Usamos suas informações para melhorar nossos serviços, enviar atualizações ou responder às suas dúvidas. Não compartilhamos suas informações com terceiros sem o seu consentimento.
+      </p>
+
+      <h2>Cookies</h2>
+      <p>
+        Utilizamos cookies para melhorar a experiência do usuário e coletar informações estatísticas sobre o uso do site.
+      </p>
+
+      <h2>Segurança</h2>
+      <p>
+        Adotamos medidas para proteger suas informações pessoais contra acesso não autorizado, alterações ou divulgações.
+      </p>
+
+      <h2>Alterações na Política</h2>
+      <p>
+        Podemos atualizar esta Política de Privacidade periodicamente. Quaisquer alterações serão publicadas nesta página.
+      </p>
+
+      <p>
+        Se você tiver dúvidas sobre esta Política de Privacidade, entre em contato conosco pelo e-mail: contato@gestaofinanceirapro.com.br.
+      </p>
+    </div>
+  );
+};
+
+export default PoliticaDePrivacidade;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,8 +16,17 @@ export default function Footer() {
           <p>Software de controle financeiro gratuito simples e eficiente para gerenciar receitas e despesas. Ele permite que você controle tudo o que entra e sai do caixa, desde vendas e pagamentos até contas e dívidas.
           </p>
         </section>
-
+     
       </footer>
+      <section className={styles.Privacidade}>
+
+<p>
+© 2024 Gestão Financeira Pro. Todos os direitos reservados. 
+<a href="/politica-de-privacidade" style={{ marginLeft: '10px', color: '#0070f3', textDecoration: 'none' }}>
+Política de Privacidade
+</a>
+</p>
+</section>
     </>
   );
 }
