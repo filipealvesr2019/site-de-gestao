b3a358b51132567d2c2edf7dc3bedfc2d674511e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-17 09:56:40 -0300

    refactor: adiciona adiciona politica de privacidade

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import { ClerkProvider } from '@clerk/nextjs';
-import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
 
    <ClerkProvider>
       <Component {...pageProps} />
-      <Head>
-      <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="abdf27a8-c0f2-4d99-8b92-e156e4821038" data-blockingmode="auto" type="text/javascript"></script>
-      </Head>
+ 
     </ClerkProvider>
 
  
