785a23f32a8e1bf4a18e128f9407c59e217ada8e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-18 08:11:32 -0300

    refactor: adiciona adiciona politica de privacidade

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1166,9 +1166,7 @@ function ProductList() {
         </>
       )}
       <Footer />
-      <Script
-      id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="abdf27a8-c0f2-4d99-8b92-e156e4821038" data-blockingmode="auto" type="text/javascript"
-      />  
+     
     </div>
   );
 }
