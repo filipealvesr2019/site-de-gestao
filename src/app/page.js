cd84fdbc9dea43f7cb553bb0a5ed1ec79c5ca984
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-07 09:19:58 -0300

    refactor: adiciona butao de redirecionamento

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       <Login />
-      <LandinPage />
+      <UserAuth />
 
       {/* <Script
       id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="abdf27a8-c0f2-4d99-8b92-e156e4821038" data-blockingmode="auto" type="text/javascript"
