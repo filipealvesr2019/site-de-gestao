563d290606176b2b9f6ee1cd9ba170bf348789fb
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-07 08:57:26 -0300

    refactor: adiciona butao de redirecionamento

--- src/app/dashboard/[plan]/page.js
+++ src/app/dashboard/[plan]/page.js
@@ -3,22 +3,11 @@ import Login from "../../Login";
 
 export default function Dashboard({params}){
     const { plan } = params || {}; // Garante que params não seja undefined
-    const handleToggleProductComponent = () =>{
-        switch(plan){
-            case "free":
-                return <ProductList />
-                
-            // case "month" || "year":
-            //     return <ProductListForSubscribed />
-                
-            default:
-                return <ProductList />
-        }
-    }
+  
     return (
         <div >
             <Login />
-        {handleToggleProductComponent()}
+        <ProductList />
 
         </div>
     )
