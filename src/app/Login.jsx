7468dd8b91c6fc442b2429d2073cf364a575cb85
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-07 08:42:17 -0300

    refactor: adiciona butao de redirecionamento

--- src/app/Login.jsx
+++ src/app/Login.jsx
@@ -19,7 +19,10 @@ export default function Login() {
   return (
     <>
       <div className={styles.login}>
+        <Link href={"https://www.gestaofinanceirapro.com.br/dashboard/free"}>
+        
         <img src="https://i.imgur.com/33Qljej.png" alt="icone do gestão financeira pro" className={styles.login__img}/>
+        </Link>
 
         <SignedOut>
           <div>
