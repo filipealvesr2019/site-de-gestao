5437bc2087de45e33f10a24a88d6a3cc0aa623b6
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-02-07 08:22:14 -0300

    feat: adiciona banners

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -642,7 +642,7 @@ function ProductList() {
   return (
     <div className={styles.container}>
 
-{/* <Link href={"https://www.profitablecpmrate.com/xtg3ptzy?key=83e76d4b462360a364a7b6b7708d01e4"}>
+<Link href={"https://www.profitablecpmrate.com/xtg3ptzy?key=83e76d4b462360a364a7b6b7708d01e4"}>
      <div className={styles.TopBannerDesktop}>
       <img src="https://i.imgur.com/PXtJII6.png" alt="" />
      </div>
@@ -650,12 +650,13 @@ function ProductList() {
 </Link>
 
 
-<Link  href={"https://www.profitablecpmrate.com/xtg3ptzy?key=83e76d4b462360a364a7b6b7708d01e4"}>
+<Link  >
      <div className={styles.TopBannerMobile}>
-      <img src="https://i.imgur.com/wzDIrMZ.png" alt="" />
+     <iframe src="//a.magsrv.com/iframe.php?idzone=5534646&size=300x250" width="300" height="250" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+
      </div>
 
-</Link> */}
+</Link> 
       {showAlert && (
         <SuccessAlert message={alertMessage} onClose={closeAlert} />
       )}
