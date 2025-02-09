c5094a08bc538a43ac2d5d207945ea8dba13f5c0
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-02-09 07:58:06 -0300

    feat: adiciona script

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,22 +8,12 @@ import {
 } from "@clerk/nextjs";
 import { ptBR } from "@clerk/localizations";
 import './globals.css';  // Certifique-se de que o arquivo de estilos globais está importado
-import Script from "next/script";
 
 // Certifique-se de envolver o ClerkProvider ao redor de todo o conteúdo
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider dynamic localization={ptBR} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html>
-      <head>
-        {/* Adicionando o Google AdSense */}
-        <Script
-          strategy="lazyOnload"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          data-ad-client="ca-pub-4020379299787957"
-          async
-        />
-      </head>
         <body>{children}</body>
       </html>
     </ClerkProvider>
