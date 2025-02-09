37f9fc728bb89e50f584b08bf4ac70f65c4751d9
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-02-09 13:00:35 -0300

    feat: adiciona ProductList

--- src/app/Login.jsx
+++ src/app/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
   return (
     <>
       <div className={styles.login}>
-        <Link href={"https://www.gestaofinanceirapro.com.br/dashboard"}>
+        <Link href={"https://www.gestaofinanceirapro.com.br"}>
         
         <img src="https://i.imgur.com/33Qljej.png" alt="icone do gestão financeira pro" className={styles.login__img}/>
         </Link>
