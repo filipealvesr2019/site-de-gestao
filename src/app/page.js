f126a3f78f0b0bc90c4ec486dee72f4cc149714d
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-02-09 07:42:35 -0300

    feat: adiciona script

--- src/app/page.js
+++ src/app/page.js
@@ -32,8 +32,11 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4020379299787957"
-     crossorigin="anonymous"></Script>
+       <Head>
+       
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4020379299787957"
+     crossorigin="anonymous"></script>
+      </Head>
    
       <Script
         id="google-analytics"
