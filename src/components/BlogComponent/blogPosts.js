4aa119bedcde5d0096a48460b825b3b084e0ff26
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-08 11:39:44 -0300

    feat: coloca blog

--- src/components/BlogComponent/blogPosts.js
+++ src/components/BlogComponent/blogPosts.js
@@ -1,12 +1,5 @@
 export const blogPosts = [
- {
-    id: 'por-tras-do-gfp',
-    title: 'A Inspiração do Zé do Queijo | Gestão Financeira Pro',
-    description: 'As palavras certas podem ser como pequenos raios de sol em dias nublados, iluminando caminhos que pareciam perdidos na escuridão.',
-    date: '08 JUN 2025',
-    category: 'Ferramentas GFP',
-    imageUrl: 'https://i.imgur.com/Wf505Su.jpg'
-},
+
 {
     id: 'comece-de-graca',
     title: 'De Graça, Comece Hoje o Caminho para Ficar no Azul',
@@ -14,5 +7,13 @@ export const blogPosts = [
     date: '08 JUN 2025',
     category: 'Educação Financeira',
     imageUrl: 'https://imgur.com/zoBUGkh.jpg'
-}
+},  
+{
+    id: 'por-tras-do-gfp',
+    title: 'A Inspiração do Zé do Queijo | Gestão Financeira Pro',
+    description: 'As palavras certas podem ser como pequenos raios de sol em dias nublados, iluminando caminhos que pareciam perdidos na escuridão.',
+    date: '08 JUN 2025',
+    category: 'Ferramentas GFP',
+    imageUrl: 'https://i.imgur.com/Wf505Su.jpg'
+},
 ]; 
