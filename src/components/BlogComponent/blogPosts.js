02862e37a4ad80f85093314012f425f0b2122fac
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-13 12:33:45 -0300

    feat: coloca blog

--- src/components/BlogComponent/blogPosts.js
+++ src/components/BlogComponent/blogPosts.js
@@ -25,7 +25,7 @@ export const blogPosts = [
     title: 'Da Bagunça ao Controle: Como o GFP pode transformar a Vida de Mulheres Vendedoras',
     description: 'Uma vendedora comum, como milhares de outras que vendem seus produtos para amigas e vizinhas no bairro, se viu em apuros. Imagine o volume de papel, de pequenas anotações, que ela acumulava. O controle do fluxo de caixa? Ah, esse era na base da sorte, um verdadeiro tiro no escuro. Se a pessoa não for organizada, a situação vira um caos.',
     date: '12 JUN 2025',
-    category: 'Tutorial',
+    category: 'Educação Financeira',
     imageUrl: 'https://imgur.com/SLVlQlp.jpg'
   }
 ]; 
